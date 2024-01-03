e3ed181ef25900840da8674692bf232f5349e5fd
Peter Stein <peterstein@protonmail.com>
2024-01-03 17:17:48 +0100

    Add Docker and Postgres support

--- README.md
+++ README.md
@@ -31,8 +31,66 @@ pip install -e .[test]
 ## Usage
 
 ```bash
-uvicorn src.mypackage.main:app
+uvicorn src.mypackage.main:app --reload
 ```
+
+### Docker
+
+If you want to run the application with inside a Docker container you have two options.
+
+#### docker-compose.yml
+
+With an applicable docker-compose.yml file in the project directory, you only need to run one command to build and start all the services:
+
+```bash
+docker-compose up --build
+```
+
+By default, docker-compose will look for files named docker-compose.yml in the project directory.
+
+
+Get a shell inside the Docker container:
+
+```bash
+docker exec -it my-container-name /bin/bash
+```
+
+
+#### Dockerfile
+
+Build the Docker image using the ```Dockerfile```. Replace my-image-name with the name you want to give to your Docker image.
+
+```bash
+docker build --tag my-image-name .
+```
+
+Run the Docker image as a container. Replace my-image-name with the name of your Docker image. 
+This command will run your container and map port 80 in the container to port 80 on your host machine.
+
+```bash
+docker run --detach --name my-container-name --publish 80:8080 my-image-name
+```
+
+To attach it use: 
+
+```bash
+docker attach my-container-name
+```
+
+To stop and remove the container do:
+
+```bash
+docker stop my-container-name
+docker rm my-container-name
+```
+
+Get a shell inside the Docker container:
+
+```bash
+docker exec -it my-container-name /bin/bash
+```
+
+
 
 ## Endpoints
 
--- src/mypackage/database.py
+++ src/mypackage/database.py
@@ -1,16 +1,47 @@
 import os
 
+from pydantic_settings import BaseSettings
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase, declarative_base
 
-DEFAULT_DATABASE_URL = "sqlite:///database.db"
-SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL", DEFAULT_DATABASE_URL)
 
-# https://fastapi.tiangolo.com/tutorial/sql-databases/#note
-engine = create_engine(
-    url=SQLALCHEMY_DATABASE_URL,
-    connect_args={"check_same_thread": False},
-)
+class Settings(BaseSettings):
+    DB_DIALECT: str = "sqlite"
+    DB_FILE: str = "database.db"
+    DB_HOST: str = "localhost"
+    DB_PORT: int = 5432
+    DB_NAME: str = "mydatabase"
+    DB_USER: str = "myuser"
+    DB_PASSWORD: str = "mypassword"
+
+
+settings = Settings()
+
+if settings.DB_DIALECT == "sqlite":
+    SQLALCHEMY_DATABASE_URL = f"{settings.DB_DIALECT}:///{settings.DB_FILE}"
+    # https://fastapi.tiangolo.com/tutorial/sql-databases/#note
+    engine = create_engine(
+        url=SQLALCHEMY_DATABASE_URL,
+        connect_args={"check_same_thread": False},
+    )
+else:
+    SQLALCHEMY_DATABASE_URL = (
+        f"{settings.DB_DIALECT}"
+        f"://"
+        f"{settings.DB_USER}"
+        f":"
+        f"{settings.DB_PASSWORD}"
+        f"@"
+        f"{settings.DB_HOST}"
+        f":"
+        f"{settings.DB_PORT}"
+        f"/"
+        f"{settings.DB_NAME}"
+    )
+    engine = create_engine(
+        url=SQLALCHEMY_DATABASE_URL,
+    )
+
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
